fixed batch size and backprop issue - still no learning

src/models.py
@@ -5,7 +5,10 @@ from torch import nn
 
 
 def prox_primal(x, tau):
-    return torch.mul(torch.sign(x), torch.max(torch.abs(x) - tau, torch.zeros_like(x)))
+    return torch.mul(
+        torch.sign(x),
+        torch.max(torch.abs(x) - tau, torch.zeros_like(x, requires_grad=False)),
+    )
 
 
 def prox_dual(y, gamma, z, rho):
@@ -22,20 +25,25 @@ class PrimalDualLayer(nn.Module):
         self.relu = nn.ReLU()
         self.linear_primal = nn.Linear(n, n, bias=False)
         self.linear_dual = nn.Linear(n, n, bias=False)
-        self.tau = self.relu(nn.Parameter(torch.randn(1)))
-        self.gamma = self.relu(nn.Parameter(torch.randn(1)))
-        self.rho = self.relu(nn.Parameter(torch.randn(1)))
+        self.tau = nn.Parameter(torch.randn(1), requires_grad=True)
+        self.gamma = nn.Parameter(torch.randn(1), requires_grad=True)
+        self.rho = nn.Parameter(torch.randn(1), requires_grad=True)
         self.H = H
 
     def forward(self, x, yk, ykm1, z):
+        breakpoint()
+        tau = self.relu(self.tau)
+        gamma = self.relu(self.gamma)
+        rho = self.relu(self.rho)
         y_tilde = 2 * yk - ykm1
-        bpk = -self.tau * torch.mm(self.H.weight.T, y_tilde)
-        x = self.linear_primal(x.reshape(-1)).reshape(-1, 1) + bpk
-        x = prox_primal(x, self.tau)
+        bpk = -self.tau * torch.mm(self.H.weight.T, y_tilde.T)
+        bpk = bpk.reshape(x.shape[0], -1)
+        x = self.linear_primal(x) + bpk
+        x = prox_primal(x, tau)
 
-        bdk = self.gamma * self.H(x.reshape(-1)).reshape(-1, 1)
-        y = self.linear_dual(yk.reshape(-1)).reshape(-1, 1) + bdk
-        y = prox_dual(y, self.gamma, z, self.rho)
+        bdk = gamma * self.H(x)
+        y = self.linear_dual(yk) + bdk
+        y = prox_dual(y, gamma, z, rho)
 
         return x, y
 
@@ -44,15 +52,15 @@ class PrimalDual(nn.Module):
     def __init__(self, n, m, n_layers):
         super().__init__()
         self.n = n
-        self.H = nn.Linear(m, n, bias=False)
+        self.H = nn.Linear(n, m, bias=False)
         self.layers = nn.ModuleList(
             [PrimalDualLayer(n, self.H) for _ in range(n_layers)]
         )
 
     def forward(self, z):
-        x = torch.zeros_like(z)
-        y = torch.zeros_like(z)
-        ykm1 = torch.zeros_like(z)
+        x = torch.zeros_like(z, requires_grad=False)
+        y = torch.zeros_like(z, requires_grad=False)
+        ykm1 = torch.zeros_like(z, requires_grad=False)
         for layer in self.layers:
             x, y = layer(x, y, ykm1, z)
             ykm1 = y

src/train.py
@@ -11,7 +11,7 @@ class PrimalDualTrainer:
             criterion = nn.MSELoss()
         self.criterion = criterion
         if optimizer is None:
-            optimizer = torch.optim.Adam(model.parameters())
+            optimizer = torch.optim.Adam(self.model.parameters())
         self.optimizer = optimizer
         if device is None:
             device = torch.device("cpu")
@@ -20,16 +20,18 @@ class PrimalDualTrainer:
     def train(self, z, x, n_epochs, batch_size=1):
         self.model.train()
         for epoch in tqdm(range(n_epochs)):
+            epoch_loss = 0
             for i in range(0, z.shape[0], batch_size):
-                z_batch = torch.tensor(z[i : max(i + batch_size, z.shape[0])]).to(
-                    self.device
-                )
-                x_batch = torch.tensor(x[i : max(i + batch_size, x.shape[0])]).to(
-                    self.device
-                )
-                for j in range(len(x_batch)):
-                    self.optimizer.zero_grad()
-                    x_pred = self.model(z_batch[j])
-                    loss = self.criterion(x_pred, x_batch[j])
-                    loss.backward(retain_graph=True)
-                    self.optimizer.step()
+                self.optimizer.zero_grad()
+                z_batch = torch.tensor(
+                    z[i : min(i + batch_size, z.shape[0])].reshape(-1, z.shape[1])
+                ).to(self.device)
+                x_batch = torch.tensor(
+                    x[i : min(i + batch_size, x.shape[0])].reshape(-1, x.shape[1])
+                ).to(self.device)
+                x_pred = self.model(z_batch)
+                batch_loss = self.criterion(x_pred, x_batch)
+                batch_loss.backward()
+                self.optimizer.step()
+                epoch_loss += batch_loss.item()
+            print(f"Epoch {epoch} loss: {epoch_loss/(z.shape[0]/batch_size)}")